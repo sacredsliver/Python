02cec6f19568e12b0980d2d7704964ebcd8dc832
sacredsliver <sliver_@mail.ru>
2022-09-23 21:49:04 +0300

    исправил 1 задачу

--- DZ1/1.py
+++ DZ1/1.py
@@ -10,7 +10,7 @@ os.system('cls')
 day = int(input('Введите номер дня недели = '))
 if (0<day<6):
     print(day, 'нет', sep=' -> ')
-elif (0>day>7):
+elif (0>=day or day>7):
     print('Такого дня недели не существует')
 else: 
     print(day, 'да', sep=' -> ')
